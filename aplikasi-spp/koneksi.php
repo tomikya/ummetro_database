293590cf2929c7059a4b2bc755e867142ef8423e
Rufi tomisila <164515299+tomikya@users.noreply.github.com>
2024-06-10 12:40:55 +0700

    Delete aplikasi-spp directory

--- aplikasi-spp/koneksi.php
+++
@@ -1,4 +0,0 @@
-<?php 
-date_default_timezone_set('Asia/Jakarta');
-$koneksi = mysqli_connect("localhost","root","", "aplikasi_spp") or die ("koneksi gagal");
-?>
--- aplikasi-spp/petugas_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$id = $_GET['id'];
-$query = mysqli_query($koneksi, "DELETE FROM petugas where id_petugas=$id");
-if ($query) {
-    echo '<script>alert("Data berhasil dihapus"); location.href="index.php?page=petugas"</script>'; 
-}else {
-    echo '<script>alert("Data GAGAL dihapus"); location.href="index.php?page=petugas"</script>'; 
-}
-?>
