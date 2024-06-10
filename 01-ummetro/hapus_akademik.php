0aa46bc40587145bbe5164103f55eda1d47fe2a0
Rufi tomisila <164515299+tomikya@users.noreply.github.com>
2024-06-10 12:41:06 +0700

    Delete 01-ummetro directory

--- 01-ummetro/hapus_akademik.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET['id'];
-$query = mysqli_query($konek, "DELETE FROM akademik where id_akademik=$id");
-if($query){
-    echo '<script>alert("Hapus Data Berhasil !"); location.href="index.php?page=akademik"</script>';
-} else {
-    echo '<script>alert("Hapus Data Gagal !"); location.href="index.php?page=akademik"</script>';
-}
-?>
--- 01-ummetro/konek.php
+++
@@ -1,3 +0,0 @@
-<?php
-$konek = mysqli_connect("localhost","root", "", "ummetro") or die ('Gabisa konek rek');
-?>
